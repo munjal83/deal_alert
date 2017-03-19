538b90cd50403ed107975022c382bc0a2b469b15
munjal patel <munjalpatel@munjal-patels-MacBook-Pro.local>
2017-03-19 12:56:39 +0530

    Adding Heroku files and reading from environ variables

--- src/models/alerts/contants.py
+++ src/models/alerts/contants.py
@@ -1,7 +1,7 @@
+import os
 
-
-URL = "https://api.mailgun.net/v3/sandbox575d2dd276574ba1a3576b888774ab71.mailgun.org"
-API_KEY = "key-7b3a5a22d9af13c1bc9a882d75611de4"
-FROM = "postmaster@sandbox575d2dd276574ba1a3576b888774ab71.mailgun.org"
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
